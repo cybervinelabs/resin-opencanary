9ffec1ce0c9768336a737cdd18ef7d497192c888
Grant Vine <grantv@cybervine.co.za>
2018-10-29 20:59:50 +0200

    .

--- configdecrypt.sh
+++ configdecrypt.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 CONFIG_PATH=`echo $RESINPOT | cut -d . -f 1 | base64 -d`
-CONFIG_MD5=`echo $RESINPOT | cut -d . -f 2 | base64 -d`
-CONFIG_DATA=`echo $RESINPOT | cut -d . -f 3 | base64 -d`
+CONFIG_SERVICE=`echo $RESINPOT | cut -d . -f 3 | base64 -d`
+CONFIG_MD5=`echo $RESINPOT | cut -d . -f 3 | base64 -d`
+CONFIG_DATA=`echo $RESINPOT | cut -d . -f 4 | base64 -d`
 
-echo $CONFIG_DATA > $CONFIG_PATH
-# md5sum $CONFIG_MD5 $CONFIG_PATH
+CMP1=`echo -n $CONFIG_DATA | md5sum | cut -d" " -f1`
+CMP2=`cat $CONFIG_PATH | md5sum | cut -d" " -f1`
 
-echo "Config Decrypt Completed" > /var/log/configdecrypt.log
+if ! cmp --silent "$CMP1" "$CMP2"; then
+  echo $CONFIG_DATA > $CONFIG_PATH
+  systemctl restart $CONFIG_SERVICE
+  echo "Config Decrypt Completed" > /var/log/configdecrypt.log
+fi
+
