020ed946a52a93f5591db5fb5aac3d85ef90fca1
Grant Vine <grantv@cybervine.co.za>
2018-10-29 21:15:34 +0200

    .

--- configdecrypt.sh
+++ configdecrypt.sh
@@ -8,7 +8,7 @@ CONFIG_DATA=`echo $RESINPOT | cut -d . -f 4 | base64 -d`
 CMP1=`echo -n $CONFIG_DATA | md5sum | cut -d" " -f1`
 CMP2=`cat $CONFIG_PATH | md5sum | cut -d" " -f1`
 
-if ! cmp --silent "$CMP1" "$CMP2"; then
+if ! [ "$CMP1" = "$CMP2" ]; then
   echo $CONFIG_DATA > $CONFIG_PATH
   systemctl restart $CONFIG_SERVICE
   echo "Config Decrypt Completed" > /var/log/configdecrypt.log
