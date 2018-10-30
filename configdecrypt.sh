e0bbecc985faad9d0ebe3ab898aedf81b7c0ba7d
Grant Vine <grantv@cybervine.co.za>
2018-10-30 17:51:38 +0200

    .

--- configdecrypt.sh
+++ configdecrypt.sh
@@ -10,10 +10,10 @@ CMP2=`cat $CONFIG_PATH | md5sum | cut -d" " -f1`
 
 if ! [ "$CMP1" = "$CMP2" ]; then
   echo $CONFIG_DATA > $CONFIG_PATH
-  if [ $RESINPOT_SCRIPT_DEBUG = "true" ]; then
-    echo $CONFIG_DATA > /var/log/configdecrypt.log
+  if [ "$RESINPOT_SCRIPT_DEBUG" = "true" ]; then
+    echo $CONFIG_DATA >> /var/log/configdecrypt.log
   fi
-  systemctl restart $CONFIG_SERVICE
-  echo $(date +%F) "Config Decrypt Completed" > /var/log/configdecrypt.log
+  # systemctl restart $CONFIG_SERVICE
+  echo $(date +%d-%h-%Y-%H-%m-%S) "Boot Config Decrypt Completed" >> /var/log/configdecrypt.log
 fi
 
