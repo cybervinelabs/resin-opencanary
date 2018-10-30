481a4d62de04aae2cc4d13680f10faf0670a4a6e
Grant Vine <grantv@cybervine.co.za>
2018-10-30 20:22:33 +0200

    .

--- configdecrypt.sh
+++ configdecrypt.sh
@@ -1,20 +1,29 @@
 #!/bin/bash
 
-CONFIG_PATH=`echo $RESINPOT | cut -d . -f 1 | base64 -d`
-CONFIG_SERVICE=`echo $RESINPOT | cut -d . -f 2 | base64 -d`
-# CONFIG_MD5=`echo $RESINPOT | cut -d . -f 3 | base64 -d`
-CONFIG_DATA=`echo $RESINPOT | cut -d . -f 4 | base64 -d`
+alias date="date +%d-%h-%Y-%H-%m-%S"
 
-CMP1=`echo -n $CONFIG_DATA | md5sum | cut -d" " -f1`
-CMP2=`cat $CONFIG_PATH | md5sum | cut -d" " -f1`
+echo $(date) "Starting Configuration Decryption Check" >> /var/log/configdecrypt.log
 
-if ! [ "$CMP1" = "$CMP2" ]; then
-  echo $(date +%d-%h-%Y-%H-%m-%S) "Configurations differ, replacing current configuration" >> /var/log/configdecrypt.log
+if ! [ [[ -z "${RESINPOT}" ]] ]; then
+  # Grab BASE64 variables from ENV 
+  echo $(date) "" >> /var/log/configdecrypt.log
+  CONFIG_PATH=`echo $RESINPOT | cut -d . -f 1 | base64 -d`
+  CONFIG_SERVICE=`echo $RESINPOT | cut -d . -f 2 | base64 -d`
+  # CONFIG_MD5=`echo $RESINPOT | cut -d . -f 3 | base64 -d`
+  CONFIG_DATA=`echo $RESINPOT | cut -d . -f 4 | base64 -d`
+
+  echo $(date) "Replacing current configuration" >> /var/log/configdecrypt.log
   echo $CONFIG_DATA > $CONFIG_PATH
+
   if [ "$RESINPOT_SCRIPT_DEBUG" = "true" ]; then
     echo $CONFIG_DATA >> /var/log/configdecrypt.log
   fi
-  # systemctl restart $CONFIG_SERVICE
-  echo $(date +%d-%h-%Y-%H-%m-%S) "Boot Config Decrypt Completed" >> /var/log/configdecrypt.log
+
+  systemctl restart $CONFIG_SERVICE
+  echo $(date) "Boot Config Decrypt Completed" >> /var/log/configdecrypt.log
+else
+  echo $(date) "$(RESINPOT) variable unset, leaving default configuration" >> /var/log/configdecrypt.log
 fi
+
+echo $(date) "Ending Configuration Decryption Check" >> /var/log/configdecrypt.log
 
