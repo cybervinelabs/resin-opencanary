8188179ddd40955e829fc1be91d1dbb20f0a49f1
Grant Vine <grantv@cybervine.co.za>
2018-10-30 21:24:12 +0200

    .

--- configdecrypt.sh
+++ configdecrypt.sh
@@ -4,25 +4,26 @@ alias date="date +%d-%h-%Y-%H-%m-%S"
 
 echo $(date) "Starting Configuration Decryption Check" >> /var/log/configdecrypt.log
 
-if ! [[ -z "${RESINPOT}" ]]; then
+
+if [ -z ${RESINPOT+x} ]; then
+  echo $(date) "Variable unset, leaving default configuration" >> /var/log/configdecrypt.log
+else
   # Grab BASE64 variables from ENV 
-  echo $(date) "" >> /var/log/configdecrypt.log
+  echo $(date) "Decoding ENV to Variables" >> /var/log/configdecrypt.log
   CONFIG_PATH=`echo $RESINPOT | cut -d . -f 1 | base64 -d`
   CONFIG_SERVICE=`echo $RESINPOT | cut -d . -f 2 | base64 -d`
   # CONFIG_MD5=`echo $RESINPOT | cut -d . -f 3 | base64 -d`
   CONFIG_DATA=`echo $RESINPOT | cut -d . -f 4 | base64 -d`
 
   echo $(date) "Replacing current configuration" >> /var/log/configdecrypt.log
-  echo $CONFIG_DATA > $CONFIG_PATH
+  echo "$CONFIG_DATA" > "$CONFIG_PATH"
 
   if [ "$RESINPOT_SCRIPT_DEBUG" = "true" ]; then
-    echo $CONFIG_DATA >> /var/log/configdecrypt.log
+    echo "$CONFIG_DATA" >> /var/log/configdecrypt.log
   fi
 
   systemctl restart $CONFIG_SERVICE
   echo $(date) "Boot Config Decrypt Completed" >> /var/log/configdecrypt.log
-else
-  echo $(date) "Variable unset, leaving default configuration" >> /var/log/configdecrypt.log
 fi
 
 echo $(date) "Ending Configuration Decryption Check" >> /var/log/configdecrypt.log
